2860e04e0db426425c875d6b7d865b85753a85c1
JundaJagad <jundajagad22@gmail.com>
2022-12-28 15:18:30 +0700

    first commit

---
+++ 2100016126_Junda Jagad_B/bebas.js
@@ -0,0 +1,9 @@
+function login(){
+   if ($("#username").val(0 == "coba" && $("#password").val() == "test") {
+      alert("You are a valid user");
+      windows.open('LatihanHome.html');
+   } else {
+      alert("You are not a valid user");
+      window.open('Latihan Login.html', '_self');
+   }
+}
